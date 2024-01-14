230536b87aff6dadd43e5d3728052db7a96b8d09
ANUMANCHI AGASTYA SAI RAM LIKHIT <78492090+astropi-b@users.noreply.github.com>
2024-01-14 17:10:52 +0530

    Update orbita.py

--- orbita.py
+++ orbita.py
@@ -1,7 +1,7 @@
-import tkinter as tk
-from tkinter import ttk
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.animation import FuncAnimation
 
